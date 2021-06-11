a39d29853e29e74e2be4a1ba9216d56fde442216
César Salcedo <cesar.salcedo@utec.edu.pe>
2021-06-11 17:38:30 -0500

    Added run.sh to better control compilation and running independently

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameter
+EXE=$1
+
+# Optional parameter
+NUM_P=$2
+
+if [[ -z $EXE ]];
+then
+	echo "error: missing argument: Provide the name of the executable"
+	exit
+fi
+
+if [[ -z $NUM_P ]];
+then
+	NUM_P=2
+fi
+
+mpirun -np $NUM_P $EXE
